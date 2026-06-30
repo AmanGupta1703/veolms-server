179aee8ae1d68e8f7264a5ff02a218d7e20ac970
Kunal Gupta <guptakunalx@gmail.com>
2026-06-30 10:32:31 +0530

    feat: add JWT access and refresh token generators

---
+++ src/utils/jwt.ts
@@ -0,0 +1,21 @@
+import jwt, { Secret, SignOptions } from "jsonwebtoken";
+
+function generateAccessToken(userId: string) {
+  const payload: { _id: string } = { _id: userId };
+  const secret = process.env.JWT_ACCESS_SECRET as Secret;
+  const options = {
+    expiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
+  } as SignOptions;
+
+  return jwt.sign(payload, secret, options);
+}
+
+function generateRefreshToken(userId: string) {
+  const payload: { _id: string } = { _id: userId };
+  const secret = process.env.JWT_REFRESH_SECRET as Secret;
+  const options = {
+    expiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
+  } as SignOptions;
+
+  return jwt.sign(payload, secret, options);
+}
